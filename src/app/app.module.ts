bc66b95201ac5e9b16cf1d59cc8d4673fd423dfe
Petar Todorov <petar.bozhidarov.todorov@gmail.com>
2019-05-16 19:30:53 +0200

    created route paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './portal/home/home.component';
+import { FooterComponent } from './footer/footer.component';
+
+// Imports
+import { RouterModule } from '@angular/router';
+import { PortalComponent } from './portal/portal.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    FooterComponent,
+    PortalComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '' , redirectTo: '/portal', pathMatch: 'full'},
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'portal', component: PortalComponent,
+        // canActivate: [AuthGuard], 
+        children: [
+        { path: '' , redirectTo: 'home', pathMatch: 'full'},
+        { path: 'home',  component: HomeComponent },
+        // { path: 'admin-list', component: AdminListComponent },
+      ]},
+      // { path: 'login', component: LoginComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
