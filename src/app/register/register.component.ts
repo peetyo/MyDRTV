6505538ad28cc838b44388262d62423be4ef6810
Adam <adborowski@gmail.com>
2019-05-17 11:32:33 +0200

    Added basic TS structure for Register, everything compiles ok

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private AuthService: AuthService, private router: Router, private formBuilder: FormBuilder,
+    private firestore: AngularFirestore) { 
+
+    this.registerForm = this.formBuilder.group({
+
+      'username': ['', Validators.required],
+      'email': ['', [Validators.required, Validators.email]],
+      'password': ['', [Validators.required]],
+      'confirmPassword': ['', [Validators.required]],
+      
+    });
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.registerForm.value);
   }
 
 }
