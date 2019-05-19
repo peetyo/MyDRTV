2e817ed48d6d0aefff152061bbcddf60c67a3d8d
Petar Todorov <petar.bozhidarov.todorov@gmail.com>
2019-05-19 22:47:38 +0200

    Rendering movielist documents to the frontend

--- src/app/portal/home/home.component.ts
+++ src/app/portal/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
+import { MovieShort } from 'src/app/models/movie-short.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  movieList: MovieShort[];
+  movieListTest: MovieShort[] = [
+    {
+    id: "testid",
+    title: "A Star Is Born",
+    thumbnail_url: "thumbnail_a_star_is_born.jpg",
+    star_rating:"8.5",
+    duration:"136",
+    genres: ["drama" , "music", "romance"]
+    },
+    {
+      id: "testid",
+      title: "A Star Is Born",
+      thumbnail_url: "thumbnail_a_star_is_born.jpg",
+      star_rating:"8.5",
+      duration:"136",
+      genres: ["drama" , "music", "romance"]
+      },
+      {
+        id: "testid",
+        title: "A Star Is Born",
+        thumbnail_url: "thumbnail_a_star_is_born.jpg",
+        star_rating:"8.5",
+        duration:"136",
+        genres: ["drama" , "music", "romance"]
+        },
+        {
+          id: "testid",
+          title: "A Star Is Born",
+          thumbnail_url: "thumbnail_a_star_is_born.jpg",
+          star_rating:"8.5",
+          duration:"136",
+          genres: ["drama" , "music", "romance"]
+          },
+  ];
+
+  constructor(private moviesService: MoviesService) { }
 
   ngOnInit() {
+    this.getMovieList('getMovieList', 'movieList');
   }
-
+  getMovieList(method,listName){
+    this.moviesService[method]().subscribe(actionArray =>{
+      this[listName] =  actionArray.map(item =>{
+        return{
+          id: item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as MovieShort
+      }) 
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDXfySvDcY_bbr4KJOEGbT7WowaEU2BqIg",
-    authDomain: "mydrtv-users-database.firebaseapp.com",
-    databaseURL: "https://mydrtv-users-database.firebaseio.com",
-    projectId: "mydrtv-users-database",
-    storageBucket: "mydrtv-users-database.appspot.com",
-    messagingSenderId: "89774255635",
-    appId: "1:89774255635:web:6332b3eb7f926658"
+    apiKey: "AIzaSyDKwA_DIoLZ8FmxaZMxa6UqoJVLmJUmAwE",
+    authDomain: "dpad-mydrtv.firebaseapp.com",
+    databaseURL: "https://dpad-mydrtv.firebaseio.com",
+    projectId: "dpad-mydrtv",
+    storageBucket: "dpad-mydrtv.appspot.com",
+    messagingSenderId: "402626031747",
+    appId: "1:402626031747:web:09faaddb47719bf8"
   }
 };
 
